91d9f9b87f6b4cd8f26e3544af02186eda9f87cc
tinnamaan <131854078+tinnamaan02@users.noreply.github.com>
2023-10-26 11:05:12 +0800

    Update script.js file
    -Menu for Hamburger Navigation

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ document.querySelector(".loader-wrapper").style.display = "flex";
 setTimeout(function () {
   document.querySelector(".loader-wrapper").style.display = "none";
 }, 2000);
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
