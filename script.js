4f93fc67b77e29d998b8bc632f3f6dbabeeceff1
aliahyaba <aliahmhae.yaba@tup.edu.ph>
2023-10-26 16:37:39 +0800

    Added go to top button
    added a go to top button and made a little adjustment in recipe page

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const goTopBtn = document.querySelector(".go-top-btn");
+
+window.addEventListener("scroll", checkHeight);
+
+function checkHeight() {
+  if (window.scrollY > 200) {
+    goTopBtn.style.display = "flex";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+}
+
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
