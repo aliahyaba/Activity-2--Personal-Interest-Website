e8d604e9af51a478e3f4da4ec3eac4721febce5e
aliahyaba <aliahmhae.yaba@tup.edu.ph>
2023-11-02 23:53:54 +0800

    Added email us in contact us page

--- script.js
+++ script.js
@@ -29,3 +29,34 @@ goTopBtn.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs
+    .sendForm(
+      "tintin",
+      "template_7949vtk",
+      "#contact-form",
+      "jEtsatNQF-FLJBevW"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "Sent!"; //if the message you want was sent
+
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        contactForm.reset();
+      },
+      () => {
+        contactMessage.textContent = "Not Sent, Try Again."; //if there is wrong with the codes on line 16-19
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
